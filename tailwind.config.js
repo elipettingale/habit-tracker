44661b1556ce17424d33ce4910730c261cd36058
Elliot Pettingale <elliot@smartercoding.co.uk>
2021-12-19 12:10:21 +0000

    setup vue and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const colors = require('tailwindcss/colors');
+
+function color_variables(name) {
+    return {
+        '50': `var(--${name}-50)`,
+        '100': `var(--${name}-100)`,
+        '200': `var(--${name}-200)`,
+        '300': `var(--${name}-300)`,
+        '400': `var(--${name}-400)`,
+        '500': `var(--${name}-500)`,
+        '600': `var(--${name}-600)`,
+        '700': `var(--${name}-700)`,
+        '800': `var(--${name}-800)`,
+        '900': `var(--${name}-900)`,
+    }
+}
+
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        colors: {
+            white: 'var(--white)',
+            gray: color_variables('gray'),
+            black: 'var(--black)',
+            pink: color_variables('pink'),
+            blue: color_variables('blue'),
+            green: color_variables('green'),
+        }
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    }
+};
